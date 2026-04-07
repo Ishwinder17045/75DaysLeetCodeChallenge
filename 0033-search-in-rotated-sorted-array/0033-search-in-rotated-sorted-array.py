8be40d364979fafe7a07f88a4c9a2ff317b61146
Ishwinder Singh <ishwinder17045@gmail.com>
2026-04-07 18:39:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -10,14 +10,12 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            # left half sorted
             if nums[left] <= nums[mid]:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-
-            # right half sorted
+           
             else:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
