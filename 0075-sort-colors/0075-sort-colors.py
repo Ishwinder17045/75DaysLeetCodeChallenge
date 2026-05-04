796be6a9ba22016f1196a73160d79df323e7addb
Ishwinder Singh <ishwinder17045@gmail.com>
2026-05-04 19:04:55 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -11,7 +11,7 @@ class Solution:
             elif nums[mid] == 1:
                 mid += 1
                 
-            else:  # nums[mid] == 2
+            else: 
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
         
